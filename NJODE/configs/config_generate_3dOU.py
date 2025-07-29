325a97c3a608e0a5dc176394ac4d237dd0bba4b3
zhaksylykov <104890692+zhaksylykov@users.noreply.github.com>
2025-07-29 12:52:35 +0200

    Create config_generate_3dOU.py
    configs for 3D OU process

---
+++ NJODE/configs/config_generate_3dOU.py
@@ -0,0 +1,116 @@
+"""
+Author: Azamat Zhaksylykov
+
+"""
+
+ 
+import numpy as np
+
+from configs.config_utils import get_parameter_array, data_path, training_data_path
+
+ 
+
+# Paths
+data_path = data_path
+training_data_path = training_data_path
+
+ 
+# Dataset parameters for Ornstein-Uhlenbeck process for Z_t
+vol_dataset_dict = {
+    'volatility': np.array([[0.2, 0.1, 0.1], [0.1, 0.25, 0.1], [0.1, 0.1, 0.3]]).tolist(),
+    'mean': np.array([1.2, 1.0, 1.5]).tolist(),
+    'speed': np.array([[0.3, 0.0, 0.0], [0.0, 0.3, 0.0], [0.0, 0.0, 0.3]]).tolist(),
+    'nb_paths': 10000,
+    'nb_steps': 100,
+    'S0': np.array([1.0, 1.5, 2.0]).tolist(),
+    'maturity': 1.0,
+    'dimension': 3,
+    'obs_perc': 1.0,
+    'scheme': 'euler',
+    'model_name': 'OrnsteinUhlenbeckForZ',
+    'dt': 0.01
+}
+
+ 
+
+# Dataset parameters for Ornstein-Uhlenbeck process for X_t
+mu_dataset_dict = {
+    'volatility': np.array([[0.2, 0.1, 0.1], [0.1, 0.25, 0.1], [0.1, 0.1, 0.3]]).tolist(),
+    'mean': np.array([1.2, 1.0, 1.5]).tolist(),
+    'speed': np.array([[0.3, 0.0, 0.0], [0.0, 0.3, 0.0], [0.0, 0.0, 0.3]]).tolist(),
+    'nb_paths': 10000,
+    'nb_steps': 100,
+    'S0': np.array([1.0, 1.5, 2.0]).tolist(),
+    'maturity': 1.0,
+    'dimension': 3,
+    'obs_perc': 1.0,
+    'scheme': 'euler',
+    'model_name': 'OrnsteinUhlenbeckMultiDimensional',
+    'dt': 0.01
+}
+
+ 
+
+ 
+
+# Train parameters for for Ornstein-Uhlenbeck process for Z_t
+vol_param_dict = {
+    'which_loss': ["easy_vol"],
+    'dataset': ['OrnsteinUhlenbeckForZ'],
+    'use_cond_exp': [False],
+    'eval_use_true_paths': [True],
+    'plot': [True],
+    'paths_to_plot': [(0,)],
+}
+
+vol_params_list = get_parameter_array(
+    param_dict=vol_param_dict)
+
+ 
+
+# Train parameters for for Ornstein-Uhlenbeck process for X_t
+mu_param_dict = {
+    'which_loss': ["easy"],
+    'dataset': ['OrnsteinUhlenbeckMultiDimensional'],
+    'plot': [True],
+    'paths_to_plot': [(0, )],
+}
+
+mu_params_list= get_parameter_array(
+    param_dict=mu_param_dict)
+
+ 
+
+ 
+
+# Model parameters for mu_model
+mu_params_dict = {
+    'input_size': 3,
+    'hidden_size': 10,
+    'output_size': 3,
+    'ode_nn': ((50, "tanh"), (50, "tanh")),
+    'readout_nn': ((50, "tanh"), (50, "tanh")),
+    'enc_nn': ((50, "tanh"), (50, "tanh")),
+    'use_rnn': False,
+    'options': {'which_loss': 'easy'},
+    "input_coords": np.arange(3),
+    "output_coords": np.arange(3),
+    "signature_coords": np.arange(3)
+}
+
+ 
+
+# Model parameters for vol_model
+vol_params_dict = {
+    'input_size': 9,
+    'hidden_size': 10,
+    'output_size': 9,
+    'ode_nn': ((50, "tanh"), (50, "tanh")),
+    'readout_nn': ((50, "tanh"), (50, "tanh")),
+    'enc_nn': ((50, "tanh"), (50, "tanh")),
+    'use_rnn': False,
+    'options': {'which_loss': 'easy_vol'},
+    "input_coords": np.arange(9),
+    "output_coords": np.arange(9),
+    "signature_coords": np.arange(9)
+}
